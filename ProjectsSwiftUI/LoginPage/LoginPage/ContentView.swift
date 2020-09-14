7f650d7c06874091bf4d4b6409cda622b0381287
Nikita Perezhogin <51512651+Rayhard@users.noreply.github.com>
2020-09-14 16:15:48 +0300

    LoginPage

---
+++ ProjectsSwiftUI/LoginPage/LoginPage/ContentView.swift
@@ -0,0 +1,304 @@
+//
+//  ContentView.swift
+//  LoginPage
+//
+//  Created by Никита Пережогин on 14.09.2020.
+//  Copyright © 2020 Nikita Perezhogin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var index = 0
+    
+    var body: some View {
+        GeometryReader{ _ in
+            VStack{
+                Image(systemName: "sun.min")
+                    .resizable()
+                    .frame(width: 70, height: 70)
+                
+                ZStack{
+                    Signup(index: self.$index)
+                        .zIndex(Double(self.index))
+                    Login(index: self.$index)
+                }
+                
+                HStack(spacing: 15){
+                    Rectangle()
+                        .fill(Color.red)
+                        .frame(height: 1)
+                    
+                    Text("OR")
+                    
+                    Rectangle()
+                        .fill(Color.red)
+                        .frame(height: 1)
+                }
+                .padding(.horizontal, 30)
+                .padding(.top, 50)
+                
+                HStack(spacing: 25){
+                    Button(action: {
+                        //
+                    }){
+                        Image(systemName: "sun.min")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                    }
+                    
+                    Button(action: {
+                        //
+                    }){
+                        Image(systemName: "sun.min")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                    }
+                    
+                    Button(action: {
+                        //
+                    }){
+                        Image(systemName: "sun.min")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                    }
+                }
+                .padding(.top, 30)
+            }
+            .padding(.vertical)
+        }
+        .background(Color.gray)
+        .edgesIgnoringSafeArea(.all)
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        return Path { path in
+            path.move(to: CGPoint(x: rect.width, y: 100))
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: 0))
+        }
+    }
+}
+
+struct CShapeRevers: Shape {
+    func path(in rect: CGRect) -> Path {
+        return Path { path in
+            path.move(to: CGPoint(x: 0, y: 100))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: rect.width, y: 0))
+        }
+    }
+}
+
+struct Login: View {
+    @State var email = ""
+    @State var pass = ""
+    @Binding var index: Int
+    
+    var body: some View{
+        ZStack(alignment: .bottom) {
+            VStack{
+                HStack{
+                    VStack(spacing: 10){
+                        Text("Login")
+                            .foregroundColor(self.index == 0 ? .white : .gray)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Capsule()
+                            .fill(self.index == 0 ? Color.blue : Color.clear)
+                            .frame(width: 100, height: 5)
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .padding(.top, 30)
+                
+                VStack{
+                    HStack(spacing: 15){
+                        Image(systemName: "mail.fill")
+                            .foregroundColor(Color.gray)
+                        
+                        TextField("Email Address", text: self.$email)
+                    }
+                    
+                    Divider()
+                        .background(Color.white.opacity(0.5))
+                }
+                .padding(.horizontal)
+                .padding(.top, 40)
+                
+                VStack{
+                    HStack(spacing: 15){
+                        Image(systemName: "eye.slash.fill")
+                            .foregroundColor(Color.gray)
+                        
+                        SecureField("Password", text: self.$pass)
+                    }
+                    
+                    Divider()
+                        .background(Color.white.opacity(0.5))
+                }
+                .padding(.horizontal)
+                .padding(.top, 30)
+                
+                HStack{
+                    Spacer(minLength: 0)
+                    
+                    Button(action: {
+                        //
+                    }) {
+                        Text("Forget Password")
+                            .foregroundColor(Color.white.opacity(0.6))
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 30)
+            }
+            .padding()
+            .padding(.bottom, 65)
+            .background(Color.pink)
+            .clipShape(CShape())
+            .contentShape(CShape())
+            .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: -5)
+            .onTapGesture {
+                self.index = 0
+            }
+            .cornerRadius(30)
+            .padding(.horizontal, 20)
+            
+            Button(action: {
+                //
+            }, label: {
+                Text("Login")
+                    .foregroundColor(Color.white)
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .padding(.horizontal, 50)
+                    .background(Color.green)
+                    .clipShape(Capsule())
+                    .shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: 5)
+            })
+                .offset(y: 35)
+                .opacity(self.index == 0 ? 1 : 0)
+        }
+    }
+}
+
+struct Signup: View {
+    @State var email = ""
+    @State var pass = ""
+    @State var repass = ""
+    @Binding var index: Int
+    
+    var body: some View{
+        ZStack(alignment: .bottom) {
+            VStack{
+                HStack{
+                    Spacer(minLength: 0)
+                    
+                    VStack(spacing: 10){
+                        Text("SignUp")
+                            .foregroundColor(self.index == 1 ? .white : .gray)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Capsule()
+                            .fill(self.index == 1 ? Color.blue : Color.clear)
+                            .frame(width: 100, height: 5)
+                    }
+                    
+                }
+                .padding(.top, 30)
+                
+                VStack{
+                    HStack(spacing: 15){
+                        Image(systemName: "envelope.fill")
+                            .foregroundColor(Color.gray)
+                        
+                        TextField("Email Address", text: self.$email)
+                    }
+                    
+                    Divider()
+                        .background(Color.white.opacity(0.5))
+                }
+                .padding(.horizontal)
+                .padding(.top, 40)
+                
+                VStack{
+                    HStack(spacing: 15){
+                        Image(systemName: "eye.slash.fill")
+                            .foregroundColor(Color.gray)
+                        
+                        SecureField("Password", text: self.$pass)
+                    }
+                    
+                    Divider()
+                        .background(Color.white.opacity(0.5))
+                }
+                .padding(.horizontal)
+                .padding(.top, 30)
+                
+                VStack{
+                    HStack(spacing: 15){
+                        Image(systemName: "eye.slash.fill")
+                            .foregroundColor(Color.gray)
+                        
+                        SecureField("Password", text: self.$repass)
+                    }
+                    
+                    Divider()
+                        .background(Color.white.opacity(0.5))
+                }
+                .padding(.horizontal)
+                .padding(.top, 30)
+            }
+            .padding()
+            .padding(.bottom, 65)
+            .background(Color.pink)
+            .clipShape(CShapeRevers())
+            .contentShape(CShapeRevers())
+            .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: -5)
+            .onTapGesture {
+                self.index = 1
+            }
+            .cornerRadius(35)
+            .padding(.horizontal, 20)
+            
+            Button(action: {
+                //
+            }, label: {
+                Text("SIGNUP")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .padding(.horizontal, 50)
+                    .background(Color.green)
+                    .clipShape(Capsule())
+                    .shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: -5)
+            })
+                .offset(y: 25)
+                .opacity(self.index == 1 ? 1 : 0)
+        }
+    }
+    
+}
